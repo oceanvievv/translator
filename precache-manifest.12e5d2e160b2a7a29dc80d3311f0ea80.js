bab370bf3e46d599ebb6783503ddb011c88edb8a
Nikolay Petrov <oceanvievv@gmail.com>
2022-01-17 12:05:20 +0500

    for the pages

---
+++ precache-manifest.12e5d2e160b2a7a29dc80d3311f0ea80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fca74300a1b31d6c357",
+    "url": "css/app.38e00e4c.css"
+  },
+  {
+    "revision": "6448acad46d0f91b69d0",
+    "url": "css/chunk-vendors.c6993f03.css"
+  },
+  {
+    "revision": "58ff2b0f7a7d869e89b539ad7753e284",
+    "url": "index.html"
+  },
+  {
+    "revision": "3fca74300a1b31d6c357",
+    "url": "js/app.8c80e329.js"
+  },
+  {
+    "revision": "6448acad46d0f91b69d0",
+    "url": "js/chunk-vendors.500154c1.js"
+  },
+  {
+    "revision": "9302b7663ab2729180093a40107feb93",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
